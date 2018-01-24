5f7676154d170818bff230c876b14507af3566a7
Elizabeth Clark <eclark2@uw.edu>
2018-01-24 14:32:17 -0800

    merge all data sets and transpose dimensions before writing a single outfile instead of one per variable

--- tools/bin/ecflow/processes/main/meteorological/get_metfcst.py
+++ tools/bin/ecflow/processes/main/meteorological/get_metfcst.py
@@ -48,6 +48,7 @@ new_units = {'was': 'm s-1', 'dps': 'degC', 'tasmean': 'degC',
 # download metdata from http://thredds.northwestknowledge.net
 met_dsets = dict()
 for model in modelnames:
+    dlist = []
     for var in varnames:
         url = ('http://tds-proxy.nkn.uidaho.edu/thredds/dodsC/' +
                'NWCSC_INTEGRATED_SCENARIOS_ALL_CLIMATE/bcsd-nmme/' +
@@ -55,7 +56,7 @@ for model in modelnames:
                'bcsd_nmme_metdata_%s_forecast_%s_daily.nc' % (
                    model, var))
         print('Reading {0}'.format(url))
-        ds = xr.open_dataset(url)
+        ds[var] = xr.open_dataset(url)
         if ds[var].attrs['units'] == 'F':
             ds[var].attrs['units'] = 'degF'
         units_in = cf_units.Unit(ds[var].attrs['units'])
@@ -63,6 +64,9 @@ for model in modelnames:
         # Perform units conversion
         units_in.convert(ds[var].values[:], units_out, inplace=True)
         ds[var].attrs['units'] = new_units[var]
-        outfile = os.path.join(met_fcst_loc, '%s_%s.nc' % (model, var))
-        print('writing {0}'.format(outfile))
-        ds.to_netcdf(outfile, mode='w', format='NETCDF4')
+        dlist.append(ds)
+    merge_ds = xr.merge(dlist)
+    merge_ds.transpose('time', 'lat', 'lon')
+    outfile = os.path.join(met_fcst_loc, '%s.nc' % (model))
+    print('writing {0}'.format(outfile))
+    merge_ds.to_netcdf(outfile, mode='w', format='NETCDF4')
